2b06f59ee93639b70b010f42c45a35efbc1c089f
masashi.ishii <ishii@mobilesupport.co.jp>
2022-07-11 07:29:27 +0000

    add follow event

--- app/Http/Controllers/LineController.php
+++ app/Http/Controllers/LineController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Services\LineService;
 use Illuminate\Http\Request;
+use Line\LINEBot\Constant\HTTPHeader;
 
 class LineController extends Controller
 {
@@ -16,7 +17,14 @@ class LineController extends Controller
 
     public function post(Request $request)
     {
-        $replyToken = $request->events[0]['replyToken'];
-        $this->lineService->SendReplyMessage($replyToken, 'サンプルメッセージ');
+        // $replyToken = $request->events[0]['replyToken'];
+        // $this->lineService->SendReplyMessage($replyToken, 'サンプルメッセージ');
+
+        $signature = $request->header(HTTPHeader::LINE_SIGNATURE);
+        if (empty($signature)) {
+            return abort(400, 'Bad Request');
+        }
+
+        $this->lineService->sortingEvent($request);
     }
 }
--- app/Services/LineService.php
+++ app/Services/LineService.php
@@ -2,6 +2,11 @@
 
 namespace App\Services;
 
+use LINE\LINEBot;
+use LINE\LINBot\Event\FollowEvent;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+
 class LineService
 {
     private $accessToken;
@@ -13,8 +18,24 @@ class LineService
     {
         $this->accessToken = $accessToken;
         $this->channleSecret = $channelSecret;
-        $this->httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($this->accessToken);
-        $this->bot = new \LINE\LINEBot($this->httpClient, ['channelSecret' => $this->channelSecret]);
+        $this->httpClient = new CurlHTTPClient($this->accessToken);
+        $this->bot = new LINEBot($this->httpClient, ['channelSecret' => $this->channelSecret]);
+    }
+
+    public function sortingEvent($request)
+    {
+        $events = $this->bot->parseEventRequet($request->getContent(), $signature);
+        foreach($events as $event)
+        {
+            if ($event instanceof TextMessage) {
+                return $this->bot->replyText($event->getReplyToken(), $event->getText());
+            }
+            if ($event instanceof FollowEvent) {
+                return $this->bot->replyText($event->getReplyToken(), '[bot]友達登録されたよ!');
+            }
+        }
+
+        return 'ok!';
     }
 
     public function SendReplyMessage($replyToken, string $text): \LINE\LINEBot\Response
