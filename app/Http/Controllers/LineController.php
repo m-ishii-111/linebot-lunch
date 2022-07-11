ddbdb7332de9045c6e2267af0da4f292785959dc
masashi.ishii <ishii@mobilesupport.co.jp>
2022-07-11 04:57:35 +0000

    fix

--- app/Http/Controllers/LineController.php
+++ app/Http/Controllers/LineController.php
@@ -11,25 +11,25 @@ class LineController extends Controller
     public function post(Request $request)
     {
         $signarure = $request->header('X-Line-Signature');
-        $validateSignature = SignatureValidator::validateSignature($httpRequestBody, $channelSecret, $signarure);
-        if ($validateSignature) {
-            return response()->json(200);
-            $bot = LineService::lineSdk();
-
-            try {
-                $events = $bot->perseEventRequest($httpRequestBody, $signature);
-                foreach ($events as $event) {
-                    // イベントごとの処理を書いていく
-                    if ($eveent instanceof LINE\LINEBot\Event\MessageEvent\TextMessage) {
-                        $bot->replyText($event->getReplyToken(), 'こんにちは!');
-                    }
-                }
-            } catch (\Exception $e) {
-                Log::debug($e);
-            }
-        } else {
+        $httpRequestBody = $request->getContent();
+        $validateSignature = SignatureValidator::validateSignature($httpRequestBody, env('LINE_CHANNEL_SECRET'), $signarure);
+        if (!$validateSignature) {
             abort(400);
         }
 
+        // return response()->json(200);
+
+        $bot = LineService::lineSdk();
+        try {
+            $events = $bot->perseEventRequest($httpRequestBody, $signature);
+            foreach ($events as $event) {
+                // イベントごとの処理を書いていく
+                if ($eveent instanceof LINE\LINEBot\Event\MessageEvent\TextMessage) {
+                    $bot->replyText($event->getReplyToken(), 'こんにちは!');
+                }
+            }
+        } catch (\Exception $e) {
+            Log::debug($e);
+        }
     }
 }
