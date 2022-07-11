2bf362790bf78cf0445791da7c8866c45b1f8b59
masashi.ishii <ishii@mobilesupport.co.jp>
2022-07-11 05:58:10 +0000

    fix syntax error

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Controllers\LineController;
+use App\Http\Controllers\LineController;
 
 /*
 |--------------------------------------------------------------------------
