9ec4024cf188f4e573d017fb60b1370a7fa9fd5b
masashi.ishii <ishii@mobilesupport.co.jp>
2022-07-12 07:34:51 +0000

    error log

--- app/Services/HotpepperService.php
+++ app/Services/HotpepperService.php
@@ -49,6 +49,7 @@ class HotpepperService
         }
 
         $response = $client->request($method, $this->baseUrl, $options);
+        error_log(print_r($response, true));
         $restaurants = json_decode($response->getBody(), true)['results'];
 
         return $restaurants;
